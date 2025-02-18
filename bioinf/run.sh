dc075f55bdb1f6ee5d41efe29f01eaf912f8a5b5
Feargus O'Gorman <feargusog@gmail.com>
2025-02-18 09:39:31 +0000

    sarek

--- bioinf/run.sh
+++ bioinf/run.sh
@@ -2,5 +2,4 @@ nextflow run nf-core/sarek \
     -profile docker \
     --input samplesheet.csv \
     --outdir ./sarek_results \
-    --step mapping \
-    --genome GRCh38
+    --genome GATK.GRCh38
